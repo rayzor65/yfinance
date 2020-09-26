0b5baeb139e8bca667fe96763b180355ecedabe2
Raymond Ho <raymond@iquitsugar.com>
2020-09-26 17:54:54 +1000

    fix empty dataframe issue

--- yfinance/base.py
+++ yfinance/base.py
@@ -281,7 +281,7 @@ class TickerBase():
         data = utils.get_json(url, proxy)
 
         # holders
-        url = "{}/{}".format(self._scrape_url, self.ticker)
+        # url = "{}/{}".format(self._scrape_url, self.ticker)
         holders = _pd.read_html(url+'/holders')
         
         if len(holders)>=3:
